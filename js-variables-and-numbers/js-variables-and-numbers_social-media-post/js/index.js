29928353313dff330205c10ef657e9b92d97a58c
Adrian Ricken <adrianricken@gmail.com>
2024-08-01 11:32:45 +0200

    change js-file

--- js-variables-and-numbers/js-variables-and-numbers_social-media-post/js/index.js
+++ js-variables-and-numbers/js-variables-and-numbers_social-media-post/js/index.js
@@ -10,7 +10,11 @@ console.clear();
 */
 
 // --v-- write your code here --v--
-
+const title = "Title: Our new product";
+const text = "Lorem Ipsum";
+let numberOfLikes = 253;
+const userWhoCreatedThisPost = "companyxy";
+const isReported = false;
 // --^-- write your code here --^--
 
 /*
@@ -18,5 +22,12 @@ console.clear();
 */
 
 // --v-- write your code here --v--
+console.log(title);
+console.log(text);
+console.log(numberOfLikes);
+console.log(userWhoCreatedThisPost);
+console.log(isReported);
 
+numberOfLikes++;
+console.log(numberOfLikes);
 // --^-- write your code here --^--
