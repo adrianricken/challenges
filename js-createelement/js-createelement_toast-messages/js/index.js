98d7dc1b73a3ad55364f6ed3f5c820a1787913e6
Adrian Ricken <adrianricken@gmail.com>
2024-08-08 12:39:40 +0200

    toast-messages: edit js file

--- js-createelement/js-createelement_toast-messages/js/index.js
+++ js-createelement/js-createelement_toast-messages/js/index.js
@@ -4,10 +4,19 @@ const toastContainer = document.querySelector('[data-js="toast-container"]');
 const addButton = document.querySelector('[data-js="add-button"]');
 const clearButton = document.querySelector('[data-js="clear-button"]');
 
-addButton.addEventListener("click", () => {
+addButton.addEventListener("click", (event) => {
   // Exercise: Append a new entry to the toast messages container
+
+  const newMessage = document.createElement("li");
+  newMessage.classList.add("toast-container__message");
+  newMessage.textContent = "I'm a toast message.";
+
+  toastContainer.append(newMessage);
+
+  return newMessage;
 });
 
 clearButton.addEventListener("click", () => {
   // Exercise: Clear the stack of toast messages
+  toastContainer.innerHTML = "";
 });
