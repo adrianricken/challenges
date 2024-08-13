3b36d458cf344a25504c41f28dd0b52150826c7b
Adrian Ricken <adrianricken@gmail.com>
2024-08-13 17:32:59 +0200

    methods: filter

--- js-array-methods/js-array-methods_filter/index.js
+++ js-array-methods/js-array-methods_filter/index.js
@@ -23,13 +23,58 @@ const cards = [
   },
 ];
 
-const onlyCardWithIdTwo = null;
+// const onlyCardWithIdTwo = null;
 
-const allCardsWith3Tags = null;
+// const allCardsWith3Tags = null;
 
-const allCardsThatAreNotBookmarked = null;
+// const allCardsThatAreNotBookmarked = null;
 
-const allCardsWithTagsHTMLOrJSThatAreBookmarked = null;
+// const allCardsWithTagsHTMLOrJSThatAreBookmarked = null;
+
+// ---------------------------------------------------------------------
+
+const onlyCardWithIdTwo = cards.filter((card) => {
+  // if (card.id == 2) {
+  //   return true;
+  // }
+  // return false;
+  return card.id == 2;
+});
+// or:
+// const onlyCardWithIdTwo = cards.filter((card) => card.id == 2);
+console.log("onlyCardWithIdTwo", onlyCardWithIdTwo);
+
+// ---------------------------------------------------------------------
+
+const allCardsWith3Tags = cards.filter((card) => {
+  if (card.tags.length === 3) {
+    return true;
+  }
+  return false;
+});
+console.log("allCardsWith3Tags", allCardsWith3Tags);
+
+// ---------------------------------------------------------------------
+
+const allCardsThatAreNotBookmarked = cards.filter(
+  (card) => card.isBookmarked === false
+);
+console.log("allCardsThatAreNotBookmarked", allCardsThatAreNotBookmarked);
+
+// ---------------------------------------------------------------------
+
+const allCardsWithTagsHTMLOrJSThatAreBookmarked = cards.filter((card) => {
+  return (
+    (card.tags.includes("html") || card.tags.includes("js")) &&
+    card.isBookmarked === true
+  );
+});
+console.log(
+  "allCardsWithTagsHTMLOrJSThatAreBookmarked",
+  allCardsWithTagsHTMLOrJSThatAreBookmarked
+);
+
+// ---------------------------------------------------------------------
 
 export {
   onlyCardWithIdTwo,
