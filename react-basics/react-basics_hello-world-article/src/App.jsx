fe5fe9e4ff649547e63f73d8c5219023367f3f9b
Adrian Ricken <adrianricken@gmail.com>
2024-08-23 16:21:43 +0200

    changed return HelloWorldArticle() into return <HelloWorldArticle />

--- react-basics/react-basics_hello-world-article/src/App.jsx
+++ react-basics/react-basics_hello-world-article/src/App.jsx
@@ -1,13 +1,13 @@
 import "./App.css";
 
 export default function App() {
-  return HelloWorldArticle();
+  return <HelloWorldArticle />;
 }
 
 function HelloWorldArticle() {
   return (
     <article>
-      <h1>BIG NEWS</h1>
+      <h1>ATTENTION</h1>
       <p>
         This is a paragraph inside an article which was created using React!
       </p>
