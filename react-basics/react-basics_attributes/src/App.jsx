932aa79ea81703cb65c4c641e663b0edeed9dd52
Adrian Ricken <adrianricken@gmail.com>
2024-08-23 16:16:36 +0200

    react-basics: attributes solution

--- react-basics/react-basics_attributes/src/App.jsx
+++ react-basics/react-basics_attributes/src/App.jsx
@@ -1,5 +1,21 @@
 import "./App.css";
 
 export default function App() {
-  return <div>Replace me with an article</div>;
+  return <Article />;
+}
+
+function Article() {
+  return (
+    <article className="article">
+      <h2 className="article__title">Wikipedia article of the day</h2>
+      <label htmlFor="inputText">Here you can type something:</label>
+      <input type="text" id="inputText"></input>
+      <a
+        href="https://de.wikipedia.org/wiki/Albrecht_Alt"
+        className="article__link"
+      >
+        Albrecht Alt (whoever that is)
+      </a>
+    </article>
+  );
 }
