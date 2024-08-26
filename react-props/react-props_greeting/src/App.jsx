1969b629491ae53e68b3571ea0fe12dd0d4e9ed0
Adrian Ricken <adrianricken@gmail.com>
2024-08-26 11:40:47 +0200

    update: delete return <div> in App Function

--- react-props/react-props_greeting/src/App.jsx
+++ react-props/react-props_greeting/src/App.jsx
@@ -1,11 +1,7 @@
 import "./App.css";
 
 export default function App() {
-  return (
-    <div>
-      <Greeting inputName={"Adrian"} />
-    </div>
-  );
+  return <Greeting inputName={"Adrian"} />;
 }
 
 function Greeting({ inputName }) {
