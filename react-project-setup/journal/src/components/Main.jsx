a1ae574804292b225cad14e79aaffdd18f8b2851
Adrian Ricken <adrianricken@gmail.com>
2024-08-27 11:54:58 +0200

    test commit

---
+++ react-project-setup/journal/src/components/Main.jsx
@@ -0,0 +1,13 @@
+import "./Main.css";
+
+export default function Main() {
+  return (
+    <body>
+      <h4 className="newHeading">NEW ENTRY</h4>
+      <input type="text" className="inputField"></input>
+      <input type="text" className="inputField"></input>
+      <br />
+      <button type="submit">Create</button>
+    </body>
+  );
+}
