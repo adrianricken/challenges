52c17315dd4563688d3b4d9ad67902303205633a
Adrian Ricken <adrianricken@gmail.com>
2024-08-28 16:45:42 +0200

    react-state-2: table reservation solution

--- react-state-2/react-state-2_table-reservation/src/App.jsx
+++ react-state-2/react-state-2_table-reservation/src/App.jsx
@@ -1,12 +1,26 @@
 import Counter from "./components/Counter";
 import "./App.css";
+import { useState } from "react";
 
 export default function App() {
+  // add state 'people' (same as in Counter but individual) with initial value 0
+  // setPeople is used to update the state
+  const [people, setPeople] = useState(0);
+
+  // add updatePeople function
+  // if people >= 0 (prevents the value from getting under 0)
+  // used to update value of people, depending on the clicked buttons from Counter component
+  const updatePeople = (people) => {
+    if (people >= 0) setPeople(people);
+  };
+
+  // return UI
   return (
     <div className="container">
       <h1>Place a Table Reservation</h1>
-      <Counter />
-      <p>You are going to reserve a table for 2 people.</p>
+      {/* render 'Counter' component and pass {updatePeople} function as prop for onCounterChange -> here the updated value is beeing sent by Counter component */}
+      <Counter onCounterChange={updatePeople} />
+      <p>You are going to reserve a table for {people} people.</p>
     </div>
   );
 }
--- react-state-2/react-state-2_table-reservation/src/components/Counter.jsx
+++ react-state-2/react-state-2_table-reservation/src/components/Counter.jsx
@@ -1,9 +1,19 @@
 import { useState } from "react";
 import "./Counter.css";
 
-
-export default function Counter() {
+// receiving {onCounterChange} as prop, which displays the 'updatePeople' function
+export default function Counter({ onCounterChange }) {
+  // add state 'people' (same as in App but individual) with initial value 0
+  // setPeople is used to update the state
   const [people, setPeople] = useState(0);
+  // updatePeople function updates the value of {people} (ONLY IN COUNTER COMPONENT)
+  // afterwards 'onCounterChange' function is called with argument 'people' to send new value of {people} to the App component (also here first checking, that {people} is not > 0)
+  const updatePeople = (people) => {
+    if (people >= 0) {
+      setPeople(people);
+      onCounterChange(people);
+    }
+  };
   return (
     <>
       <h2>How many people would you like to visit us with?</h2>
@@ -12,7 +22,7 @@ export default function Counter() {
           type="button"
           className="counter__button"
           aria-label="increment people count"
-          onClick={() => setPeople(people + 1)}
+          onClick={() => updatePeople(people + 1)}
         >
           +
         </button>
@@ -20,7 +30,7 @@ export default function Counter() {
           type="button"
           className="counter__button"
           aria-label="decrement people count"
-          onClick={() => setPeople(people - 1)}
+          onClick={() => updatePeople(people - 1)}
         >
           -
         </button>
