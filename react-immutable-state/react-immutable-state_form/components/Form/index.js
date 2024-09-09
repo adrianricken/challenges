c03560d29151b1dfb6f46573cf22d57c53599f0a
Adrian Ricken <adrianricken@gmail.com>
2024-09-09 12:04:01 +0200

    react-immutable-state: form solution

--- react-immutable-state/react-immutable-state_form/components/Form/index.js
+++ react-immutable-state/react-immutable-state_form/components/Form/index.js
@@ -1,8 +1,43 @@
+import { useImmer } from "use-immer";
 import { useState } from "react";
 import { StyledForm, StyledInputContainer } from "./Form.styled";
 
+// useState:
+
+// export default function Form() {
+//   const [mountain, setMountain] = useState({
+//     name: "Mount Everest",
+//     values: {
+//       altitude: 8848,
+//       mountainRange: "Himalayas",
+//     },
+//   });
+
+// function handleNameChange(event) {
+//   setMountain({
+//     ...mountain,
+//     name: event.target.value,
+//   });
+// }
+
+// function handleAltitudeChange(event) {
+//   setMountain({
+//     ...mountain,
+//     values: { ...mountain.values, altitude: event.target.value },
+//   });
+// }
+
+// function handleMountainRangeChange(event) {
+//   setMountain({
+//     ...mountain,
+//     values: { ...mountain.values, mountainRange: event.target.value },
+//   });
+// }
+
+// useImmer:
+
 export default function Form() {
-  const [mountain, setMountain] = useState({
+  const [mountain, updateMountain] = useImmer({
     name: "Mount Everest",
     values: {
       altitude: 8848,
@@ -10,11 +45,23 @@ export default function Form() {
     },
   });
 
-  function handleNameChange(event) {}
+  function handleNameChange(event) {
+    updateMountain((draft) => {
+      draft.name = event.target.value;
+    });
+  }
 
-  function handleAltitudeChange(event) {}
+  function handleAltitudeChange(event) {
+    updateMountain((draft) => {
+      draft.values.altitude = event.target.value;
+    });
+  }
 
-  function handleMountainRangeChange(event) {}
+  function handleMountainRangeChange(event) {
+    updateMountain((draft) => {
+      draft.values.mountainRange = event.target.value;
+    });
+  }
 
   return (
     <StyledForm>
