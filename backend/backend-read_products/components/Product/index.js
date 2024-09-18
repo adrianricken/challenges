4867c5ec1533a3935c3e0ddd2cf8a4e69a02c5b3
Adrian Ricken <adrianricken@gmail.com>
2024-09-18 16:30:25 +0200

    resolve backend-read challenge

--- backend/backend-read_products/components/Product/index.js
+++ backend/backend-read_products/components/Product/index.js
@@ -2,21 +2,16 @@ import useSWR from "swr";
 import { useRouter } from "next/router";
 import { ProductCard } from "./Product.styled";
 import { StyledLink } from "../Link/Link.styled";
-
 export default function Product() {
   const router = useRouter();
   const { id } = router.query;
-
   const { data, isLoading } = useSWR(`/api/products/${id}`);
-
   if (isLoading) {
     return <h1>Loading...</h1>;
   }
-
   if (!data) {
     return;
   }
-
   return (
     <ProductCard>
       <h2>{data.name}</h2>
@@ -24,6 +19,28 @@ export default function Product() {
       <p>
         Price: {data.price} {data.currency}
       </p>
+      <div>
+        <br />
+        {data.reviews.length === 0 ? (
+          <>
+            <p style={{ fontStyle: "italic" }}>No reviews yet</p>
+            <br />
+          </>
+        ) : (
+          data.reviews.map((review, index) => (
+            <>
+              <p key={index}>
+                <strong>{review.title}</strong>
+                <br />
+                {review.text}
+                <br />
+                Rating: {review.rating} / 5
+              </p>{" "}
+              <br />
+            </>
+          ))
+        )}
+      </div>
       <StyledLink href="/">Back to all</StyledLink>
     </ProductCard>
   );
--- backend/backend-read_products/components/ProductList/index.js
+++ backend/backend-read_products/components/ProductList/index.js
@@ -1,25 +1,21 @@
 import useSWR from "swr";
 import { StyledHeading, StyledList } from "./ProductList.styled";
 import { StyledLink } from "../Link/Link.styled";
-
 export default function ProductList() {
   const { data, isLoading } = useSWR("/api/products");
-
   if (isLoading) {
     return <h1>Loading...</h1>;
   }
-
   if (!data) {
     return;
   }
-
   return (
     <>
       <StyledHeading>Available Fishes</StyledHeading>
       <StyledList>
         {data.map((product) => (
-          <li key={product.id}>
-            <StyledLink href={`/${product.id}`}>{product.name}</StyledLink>
+          <li key={product._id}>
+            <StyledLink href={`/${product._id}`}>{product.name}</StyledLink>
           </li>
         ))}
       </StyledList>
