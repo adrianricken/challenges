0a0766014f0fadf0eb92d8dd5590dda0569269f6
Adrian Ricken <adrianricken@gmail.com>
2024-09-18 18:12:54 +0200

    delete directory backend-api-routes

--- backend-api-routes/backend-api-routes_products/pages/api/products/[id].js
+++
@@ -1,6 +0,0 @@
-import { getProductById } from "@/services/productServices";
-
-export default function handler(request, response) {
-  const { id } = request.query;
-  response.status(200).json(getProductById(id));
-}
